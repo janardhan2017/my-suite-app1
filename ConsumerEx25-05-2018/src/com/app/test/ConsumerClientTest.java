3e58f3fa91d3b9db6861d1ecf1607e7ba56cd939
Janardhan <Janardhan@Janardhan-PC>
2020-12-06 00:55:05 +0530

    followed coding standard

--- ConsumerEx25-05-2018/src/com/app/test/ConsumerClientTest.java
+++ ConsumerEx25-05-2018/src/com/app/test/ConsumerClientTest.java
@@ -5,18 +5,18 @@ import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
 
 public class ConsumerClientTest {
-	
+
 	public static void main(String[] args) {
-		String url="http://localhost:2500/ProviderEx25-05-2018/rest/msg/show";
-		Client c=Client.create();
-		WebResource wr=c.resource(url);
-		ClientResponse cr=wr.get(ClientResponse.class);
-		String str=cr.getEntity(String.class);
+		String url = "http://localhost:2500/ProviderEx25-05-2018/rest/msg/show";
+		Client cient = Client.create();
+		WebResource wr = cient.resource(url);
+		ClientResponse cr = wr.get(ClientResponse.class);
+		String str = cr.getEntity(String.class);
 		System.out.println(str);
 		System.out.println(cr.getStatus());
 		System.out.println(cr.getStatusInfo());
-		
-		
+		System.out.println("janardhan");
+
 	}
 
 }
